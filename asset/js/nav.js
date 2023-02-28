69216a8b70f03f5f5ce310f8fee0accefda6e7fb
Emy-05 <106020732+Emy-05@users.noreply.github.com>
2023-02-28 14:57:58 +0100

    Update nav.js

--- asset/js/nav.js
+++ asset/js/nav.js
@@ -1,1 +1,13 @@
+let toggle = document.querySelector('.toggle');
+let navLinks = document.querySelector('.nav-links');
 
+toggle.addEventListener('click', () => {
+    toggle.classList.toggle('open');
+    navLinks.classList.toggle('open');
+})
+
+document.querySelectorAll(".nav-links").forEach(n => n.
+    addEventListener('click', () => {
+        toggle.classList.remove('open');
+        navLinks.classList.remove('open');
+    }))
